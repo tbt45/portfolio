fb4ed9f4402d7fef946105a46613b5d35b18eea2
tbt45 <katsu5843_0717@yahoo.co.jp>
2021-08-20 23:03:48 +0900

    secondedit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import test from './test.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component("test",test);
+// testをグローバル登録
 
 new Vue({
   render: h => h(App),
