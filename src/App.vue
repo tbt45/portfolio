6d30a4a055e449dd2a1183b6f7c3039633a9e230
tbt45 <katsu5843_0717@yahoo.co.jp>
2021-08-24 23:21:56 +0900

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,70 @@
 <template>
-  <div>
-  <p class="title">vue.js試験運用</p>
-  <Test-second></Test-second>
-  <Test @add-todo="addTodo"></Test>
+  <!-- <div>
+    <p class="title">vue.js試験運用</p>
+    <Test-second></Test-second>
+    <Test></Test>
+  </div> -->
+    <div id="app">
+    <h1>Todo</h1>
+    <div>
+      <input type="text" v-model="title" placeholder="タイトル">
+    </div>
+    <div>
+
+      <textarea v-model="body" placeholder="内容"></textarea>
+    </div>
+    <div>
+      <input type="submit" value="追加" @click="addList">
+    </div>
+    <ul v-for="(list, i) in lists" v-bind:key="i">
+      <li>id: {{i}}, title: {{ list.title }} <button @click="deleteList(i)">削除</button></li>
+    </ul>
+    <pre>
+      {{$data}}
+    </pre>
   </div>
 </template>
 
 <script>
-import Test from "./components/Test.vue";
-import TestSecond from './components/TestSecond.vue';
+// import Test from "./components/Test.vue";
+// import TestSecond from './components/TestSecond.vue';
 
 export default {
-  components: {
-    Test,
-    TestSecond,
-  },
-  methods:{
-    addTodo(Test) {
-      const newTodo = Test && Test.trim();
-      if (!newTodo){
-        return;
-      }
-      this.todo.title = newTodo;
+    data: () => ({
+    lists: [
+      {title: 'mytodo', body: 'contentcontent'},
+      {title: 'hogehoge', body: 'yuuyuuyuyuyu'}
+    ],
+    title: '',
+    body: ''
+  }),
+  methods: {
+    // リストの追加
+    addList: function(){
+      if(this.title === '' || this.body === '') return
+      this.lists.push({title: this.title, body: this.body})
+      this.title = ''
+      this.body = ''
+    },
+    // リストの削除
+    deleteList: function(i){
+      this.lists.splice(i,1)
     }
   }
-}
+};
+//   data (){
+//   },
+//   components: {
+//     Test,
+//     TestSecond,
+//   },
+// }
 </script>
 
 <style>
-  .title{
+  /* .title{
     font-size: 50px;
     background-color: aqua;
   }
-  
+   */
 </style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,32 +1,50 @@
 <template>
-  <div>
-    <input
-      v-model="newTodo" 
-      @keydown.enter="addTodo">
-    <button @click="OnClick">入力</button>
-    <p>[{{ newTodo }}]</p>
+  <div id="app">
+    <h1>MyTodoApp</h1>
+    <div>
+      <label for="title">タイトル：</label>
+      <input type="text" v-model="title" placeholder="タイトル">
+    </div>
+    <div>
+      <label for="body">内容：</label>
+      <textarea v-model="body" placeholder="内容"></textarea>
+    </div>
+    <div>
+      <input type="submit" value="追加" @click="addList">
+    </div>
+    <ul v-for="(list, i) in lists" v-bind:key="i">
+      <li>id: {{i}}, title: {{ list.title }} <button @click="deleteList(i)">削除</button></li>
+    </ul>
+    <pre>
+      {{$data}}
+    </pre>
   </div>
 </template>
 
 <script>
+
 export default {
-  // el: '#app',
-  data: function(){
-    return {
-      newTodo: '',
-    };
-  },
+  data: () => ({
+    lists: [
+      {title: 'mytodo', body: 'contentcontent'},
+      {title: 'hogehoge', body: 'yuuyuuyuyuyu'}
+    ],
+    title: '',
+    body: ''
+  }),
   methods: {
-    addTodo(){
-      this.$emit('add-todo',this.newTodo);
-      this.newTodo = '';
+    // リストの追加
+    addList: function(){
+      if(this.title === '' || this.body === '') return
+
+      this.lists.push({title: this.title, body: this.body})
+      this.title = ''
+      this.body = ''
+    },
+    // リストの削除
+    deleteList: function(i){
+      this.lists.splice(i,1)
     }
   }
-}
+};
 </script>
-
-<style>
-#app{
-  color: red;
-}
-</style>
