a931f45e5945dfebcd04835e4487bdd28236f5cc
tbt45 <katsu5843_0717@yahoo.co.jp>
2021-09-02 23:36:11 +0900

    README setting

--- src/components/views/Test.vue
+++ src/components/views/Test.vue
@@ -1,8 +1,9 @@
 <template>
     <div> 
         <button @click="Home">ホーム</button>
-        <div class="bg-color">
-            <button @click="increment">ボタン</button>
+        <!-- <div class="bg-color">
+            <button @click="increment">+1</button>
+            <button @click="decrement">-1</button>
             <p>いいね({{ number }})</p>
             <p>入力↓</p>
             <input v-model="message" placeholder="入力してください">
@@ -16,30 +17,69 @@
             <br>
             <span>チェック {{ check }}</span>
         </div>
-        <p class="end">テスト終わり</p>
+        <p class="end">テスト終わり</p> -->
+        <h2>テスト掲示板</h2>
+        <label for="name" class="name">名前</label>
+        <input type="text" id="name" v-model="name">
+        <label for="comment" class="name">コメント</label>
+        <textarea name="comment" id="comment" class="name" v-model="comment"></textarea>
+        <button @click="createComment" class="name">送信</button>
+        <h2>掲示板</h2>
+        <div v-for="post in posts" :key="post.name" class="comment">
+            <div>名前 :{{post.fields.name.stringValue}}</div>
+            <div>コメント :{{post.fields.comment.stringValue}}</div>
+        </div>
     </div>
 </template>
 
 <script>
-
-
+import axios from "axios";
 export default {
     data: function() {
         return {
-        message: [],
-        check: [],
-        number: 0,
-        }
+            // message: [],
+            // check: [],
+            // number: 0,
+            name: "",
+            comment: "",
+            posts: []
+        };
+    },
+    created() {
+        axios.get(
+            'https://firestore.googleapis.com/v1/projects/test-74b65/databases/(default)/documents/comments'
+        ).then(response => {
+            this.posts = response.data.documents;
+        });
     },
     methods: {
-        increment(){
-        this.number += 1;
-        },
+        // increment(){
+        //     this.number += 1;
+        // },
+        // decrement(){
+        //     this.number -= 1;
+        // },
         Home() {
             this.$router.push("/");
         },
+        createComment(){
+            axios.post('https://firestore.googleapis.com/v1/projects/test-74b65/databases/(default)/documents/comments',
+            {
+                fields: {
+                    name: {
+                        stringValue: this.name
+                    },
+                    comment: {
+                        stringValue: this.comment
+                    }
+                }
+            }
+            )
+        this.name = "";
+        this.comment = "";
+        }
     }
-} 
+};
 </script>
 
 <style>
@@ -49,5 +89,12 @@ export default {
     .end{
         background-color: rgb(175, 175, 241);
         margin-bottom: 100px;
+    }
+    .name{
+        display: block;
+    }
+    .comment{
+        color: crimson;
+        
     }
 </style>
